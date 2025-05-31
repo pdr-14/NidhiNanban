c667fb4831020f5b7c0e0ab7dfecc5918508a7f8
pdr-14 <pdr140620@outlook.com>
2025-05-31 12:08:30 +0530

    Added New Class

--- Nidhinanban/Services/InterestService.cs
+++ Nidhinanban/Services/InterestService.cs
@@ -4,14 +4,27 @@ using Microsoft.Extensions.Diagnostics;
 namespace Nidhinanban.Services
 {
     public class InterestService() {
-        public (string principleamount,string interestamount,string totalamount) calculateinterest(float principleamount,float interest,float year,float month,float week,string type){
+        public async Task< (string principleamount,string interestamount,string totalamount,List<string> weekstable)> calculateinterest(float principleamount,float interest,float year,float month,float week,string type){
             float principleamounts=principleamount;
             float interestamounts=0f;
             float totalamounts=0f;
+            List<string> datas=new List<string>();
             if(type.ToLower()=="week")
             {
                 interestamounts=(float) Math.Round(principleamount*interest*week/(100*52),2);
                 totalamounts=(float) Math.Round(principleamounts+interestamounts,2);
+                
+            await Task.Run(() =>
+            {
+                DateTime startingdate = DateTime.Now;
+                int numberofweeks = 4;
+                
+                for (int i = 0; i < numberofweeks; i++)
+                {
+                    DateTime enddate = startingdate.AddDays(i * 7);
+                    datas.Add(enddate.ToString("dd-MM-yyyy"));
+                }
+            });
             }
             else if(type.ToLower()=="month")
             {
@@ -23,7 +36,7 @@ namespace Nidhinanban.Services
                 interestamounts=(float) Math.Round(principleamount*interest*year/100,2);
                 totalamounts= (float) Math.Round(principleamounts+interestamounts,2);
             }
-            return("₹"+principleamounts,"₹"+interestamounts,"₹"+totalamounts);
+            return("₹"+principleamounts,"₹"+interestamounts,"₹"+totalamounts,datas);
         }
     }
 }
