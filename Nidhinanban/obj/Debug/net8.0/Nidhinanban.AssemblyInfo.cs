9611fda588d41408248358463871a5127de06187
pdr-14 <pdr140620@outlook.com>
2025-06-11 19:42:28 +0530

    Trail Image Upload Completed

--- Nidhinanban/obj/Debug/net8.0/Nidhinanban.AssemblyInfo.cs
+++ Nidhinanban/obj/Debug/net8.0/Nidhinanban.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Nidhinanban")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdae68e2e52b0ad0c1f0cdbf7a7570546fa09578")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a030d269c4b7cb77a4a9da7f2b14407884acb061")]
 [assembly: System.Reflection.AssemblyProductAttribute("Nidhinanban")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Nidhinanban")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
